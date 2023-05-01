970a34524421f4cd06cce1ceddbc4471eab70003
DosAb <aiboldosonov@gmail.com>
2023-05-01 11:14:55 +0600

    add lightMap

--- src/Scene.js
+++ src/Scene.js
@@ -1,11 +1,16 @@
-import { useAnimations, useTexture, useGLTF, useVideoTexture } from "@react-three/drei"
-import { useFrame } from "@react-three/fiber"
+import { shaderMaterial, useAnimations, useTexture, useGLTF, useVideoTexture, Float } from "@react-three/drei"
+import { extend, useFrame } from "@react-three/fiber"
 import { useRef } from "react"
 import * as THREE from 'three'
+
+import vertexShader from './shaders/vertex'
+import fragmentShader from "./shaders/fragment"
+
 
 
 export default function Scene()
 {
+    const tempArray = [...Array(20)]
 
     const model = useGLTF('./mobileBot.glb')
     const botGeometry = model.nodes.bot.geometry
@@ -13,16 +18,51 @@ export default function Scene()
 
     const botBakedTexture = useTexture('./colorBaked2.jpg')
     botBakedTexture.flipY = false
+    const lightMapTexture = useTexture('./lightMapBake.jpg')
+    lightMapTexture.flipY = false
+    // botBakedTexture.encoding = THREE.sRGBEncoding
+    // botBakedTexture.mapping = THREE.ACESFilmicToneMapping
 
     const videoTexture = useVideoTexture("./vintageVideo.mp4")
     videoTexture.flipY = false
 
-    return <group rotation-y={Math.PI} >
-        <mesh  geometry={botGeometry} >
-            <meshBasicMaterial map={botBakedTexture} />
-        </mesh>
-        <mesh geometry={windowGeometry} >
-            <meshBasicMaterial map={videoTexture} />
-        </mesh>
-    </group>  
+    const BotMaterial = shaderMaterial(
+        {
+            uBakedTexture: botBakedTexture,
+            ulightMapTexture: lightMapTexture,
+        },
+        vertexShader,
+        fragmentShader,
+    )
+    
+    extend({ BotMaterial })
+
+    return<>
+    <Float
+        speed={1} // Animation speed, defaults to 1
+        rotationIntensity={0.4} // XYZ rotation intensity, defaults to 1
+        floatIntensity={0.5} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+        floatingRange={[1, 2]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+        >
+            <group rotation-y={Math.PI} >
+                <mesh  geometry={botGeometry} >
+                    <botMaterial />
+                    {/* <meshBasicMaterial map={botBakedTexture} /> */}
+                </mesh>
+                <mesh geometry={windowGeometry} >
+                    <meshBasicMaterial map={videoTexture} />
+                </mesh>
+            </group>  
+    </Float>
+
+    {/* {tempArray.map((value, index)=>{
+        return<>
+            <mesh position={[(Math.random() - 0.5) * 40 + (Math.random() < 0.5? -1 : 1),(Math.random() - 0.5) * 40 + (Math.random() < 0.5? -1 : 1),(Math.random() - 0.5) * 40 + (Math.random() < 0.5? -1 : 1)] } key={index}>
+                <boxGeometry />
+                <meshNormalMaterial />
+            </mesh>
+        </>
+    })} */}
+
+    </> 
 }
--- src/shaders/fragment.js
+++ src/shaders/fragment.js
@@ -1,11 +1,18 @@
 export default /*glsl */
-`uniform sampler2D uMask;
+`
+uniform sampler2D uBakedTexture;
+uniform sampler2D ulightMapTexture;
 varying vec2 vUv;
-varying float vAlpha;
 
 void main()
 {
-    float maskStrength = texture2D(uMask, gl_PointCoord).r;
-    vec3 texture = texture2D(uMask, gl_PointCoord).rgb;
-    gl_FragColor = vec4(1.0, 1.0, 1.0, maskStrength * vAlpha);
+
+    vec3 lightTexture = texture2D(ulightMapTexture, vUv).rgb;
+    vec3 texture = texture2D(uBakedTexture, vUv).rgb;
+
+    vec3 lightColor = vec3(255.0 / 192.0, 255.0 /218.0, 255.0 / 116.0);
+
+    vec3 mixColor = mix(texture, lightColor, lightTexture.r * 0.5);
+
+    gl_FragColor = vec4(mixColor, 1.0);
 }`
